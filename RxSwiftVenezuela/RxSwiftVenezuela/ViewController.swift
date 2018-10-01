3371faff4ee9f9029fe9cde19f4ed55f6e601c39
Ernesto <ecolmenarez@teravisiontech.com>
2018-10-01 11:32:15 -0400

    Initial Project

---
+++ RxSwiftVenezuela/RxSwiftVenezuela/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftVenezuela
+//
+//  Created by TerAdmin on 10/1/18.
+//  Copyright © 2018 Ernesto Colmenarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
